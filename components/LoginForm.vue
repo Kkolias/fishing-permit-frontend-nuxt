564e93b44299411c22faf4fceab44f9236a64d67
Elias Koivuporras <eppu2002@gmail.com>
2024-01-07 14:26:31 +0200

    loginForm sign-up link

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -2,25 +2,42 @@
   <div class="component-LoginForm">
     <div class="login-form-wrapper">
       <h1>Kirjaudu</h1>
-      <form @submit.prevent="e => submit(e)">
+      <form @submit.prevent="(e) => submit(e)">
         <div v-if="errorMessage.length" class="error-wrapper">
           <p class="error-message">{{ errorMessage }}</p>
         </div>
 
         <div class="input-wrapper">
           <label for="email">Sähköposti:</label>
-          <input v-model="email" type="text" id="email" @click="setErrorMessage('')">
+          <input
+            v-model="email"
+            type="text"
+            id="email"
+            @click="setErrorMessage('')"
+          />
         </div>
         <div class="input-wrapper">
           <label for="psw">Salasana:</label>
-          <input v-model="password" type="password" id="psw" @click="setErrorMessage('')">
+          <input
+            v-model="password"
+            type="password"
+            id="psw"
+            @click="setErrorMessage('')"
+          />
         </div>
-        <div class="button-wrapper">
-          <ButtonWithLoader buttonType="submit" :loading="loading" @click="submit">
-            <span>
-              Kirjaudu
-            </span>
-          </ButtonWithLoader>
+        <div class="bottom-wrapper">
+          <p class="new-user">
+            Ei käyttäjää? luo uusi käyttäjä <NuxtLink to="sign-up" class="href-btn">tästä</NuxtLink>
+          </p>
+          <div class="button-wrapper">
+            <ButtonWithLoader
+              buttonType="submit"
+              :loading="loading"
+              @click="submit"
+            >
+              <span> Kirjaudu </span>
+            </ButtonWithLoader>
+          </div>
         </div>
       </form>
     </div>
@@ -28,64 +45,63 @@
 </template>
 
 <script lang="ts">
-import apiUser from '~/utils/api-user'
+import apiUser from "~/utils/api-user";
 
 interface LoginFormData {
-  email: string,
-  password: string,
-  errorMessage: string,
-  loading: boolean
+  email: string;
+  password: string;
+  errorMessage: string;
+  loading: boolean;
 }
 
 const ERROR = {
-  WRONG_PASSWORD: 'wrong password',
-  NO_USER_FOUND: 'no user found'
-}
+  WRONG_PASSWORD: "wrong password",
+  NO_USER_FOUND: "no user found",
+};
 
 export default {
   data: (): LoginFormData => ({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
 
-    errorMessage: '',
+    errorMessage: "",
 
-    loading: false
+    loading: false,
   }),
   methods: {
     async submit(e: Event) {
-      if (this.loading) return
-      this.setLoading(true)
-      e.preventDefault()
+      if (this.loading) return;
+      this.setLoading(true);
+      e.preventDefault();
 
-      const error = await apiUser.login(this.email, this.password)
+      const error = await apiUser.login(this.email, this.password);
 
       if (error?.length) {
-        console.log(error === ERROR.WRONG_PASSWORD)
+        console.log(error === ERROR.WRONG_PASSWORD);
         if (error === ERROR.WRONG_PASSWORD) {
-          this.setErrorMessage('Antamasi salasana on väärä.')
-        }
-        else if (error === ERROR.NO_USER_FOUND) {
-          this.setErrorMessage('Sähköpostilla ei löydy käyttäjää.')
-        }
-        else {
-          this.setErrorMessage('Virhe kirjautuessa palveluun. Yritä uudelleen.')
+          this.setErrorMessage("Antamasi salasana on väärä.");
+        } else if (error === ERROR.NO_USER_FOUND) {
+          this.setErrorMessage("Sähköpostilla ei löydy käyttäjää.");
+        } else {
+          this.setErrorMessage(
+            "Virhe kirjautuessa palveluun. Yritä uudelleen."
+          );
         }
       } else {
-        this.setErrorMessage('')
-        this.$router.push("/")
+        this.setErrorMessage("");
+        this.$router.push("/");
       }
-      this.setLoading(false)
+      this.setLoading(false);
     },
     setLoading(value: boolean): void {
-      this.loading = value
+      this.loading = value;
     },
     setErrorMessage(message: string): void {
-      this.errorMessage = message
-    }
-  }
-}
+      this.errorMessage = message;
+    },
+  },
+};
 </script>
-
 
 <style lang="less" scoped>
 .component-LoginForm {
@@ -95,7 +111,6 @@ export default {
   transform: translate(-50%, -50%);
 
   .login-form-wrapper {
-
     border: 2px solid var(--black-2);
     border-radius: 10px;
     padding: 24px;
@@ -120,7 +135,6 @@ export default {
         }
       }
 
-
       .input-wrapper {
         display: flex;
         flex-direction: column;
@@ -132,15 +146,29 @@ export default {
         }
       }
 
-      .button-wrapper {
-        margin-top: 4px;
-        width: 100%;
+      .bottom-wrapper {
         display: flex;
-        justify-content: flex-end;
+        justify-content: space-between;
+        .new-user {
+          font-size: 14px;
+          text-align: left;
+          text-wrap: nowrap;
+
+          .href-btn {
+            color: var(--green-1);
+          }
+        }
+
+        .button-wrapper {
+          margin-top: 4px;
+          width: 100%;
+          display: flex;
+          justify-content: flex-end;
+          
+          
+        }
       }
     }
-
-
   }
 }
 </style>
